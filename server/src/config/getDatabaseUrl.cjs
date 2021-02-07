1aab1ada7b419077540235568d4d0532bc1dc25d
TurtIeSocks <58572875+TurtIeSocks@users.noreply.github.com>
2021-02-07 14:22:35 -0500

    Initialize

---
+++ server/src/config/getDatabaseUrl.cjs
@@ -0,0 +1,11 @@
+const getDatabaseUrl = (nodeEnv) => {
+  return (
+    {
+      development: "postgres://postgres:postgres@localhost:5432/launch-social_development",
+      test: "postgres://postgres:postgres@localhost:5432/launch-social_test",
+      e2e: "postgres://postgres:postgres@localhost:5432/launch-social_e2e",
+    }[nodeEnv] || process.env.DATABASE_URL
+  );
+};
+
+module.exports = getDatabaseUrl;
